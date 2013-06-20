c8f6a76846fb9f16053a0cce492670b149aca570
Philippe_Wang <Philippe.Wang@gmail.com>
2013-06-20 16:28:57 +0100

    mpp_charset.ml

--- src/mpp_charset.ml
+++ src/mpp_charset.ml
@@ -8,3 +8,14 @@ include Set.Make(Char)
 
 let of_list l =
   List.fold_left (fun r e -> add e r) empty l 
+
+let to_string s =
+  let b = Buffer.create 42 in
+    iter (Buffer.add_char b) s;
+    Buffer.contents b
+
+let to_escaped_string s =
+  let b = Buffer.create 42 in
+    iter (fun c -> Buffer.add_string b (Char.escaped c)) s;
+    Buffer.contents b
+
