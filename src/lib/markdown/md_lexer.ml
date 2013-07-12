d886c7c22c34f2693b0ec0efa939b8843835a62f
Philippe Wang <philippe.wang@gmail.com>
2013-07-12 15:22:32 +0100

    polymorphic variant replaced by monomorphic variant

--- src/lib/markdown/md_lexer.ml
+++ src/lib/markdown/md_lexer.ml
@@ -17,6 +17,43 @@
 
 *)
 
+type t =
+  | Ampersand of int
+  | At of int
+  | Backquote of int
+  | Backslash of int
+  | Bar of int
+  | Caret of int
+  | Cbrace of int
+  | Colon of int
+  | Cparenthesis of int
+  | Csbracket of int
+  | Dollar of int
+  | Dot of int
+  | Doublequote of int
+  | Exclamation of int
+  | Greaterthan of int
+  | Hash of int
+  | Lessthan of int
+  | Minus of int
+  | Newline of int
+  | Number of string
+  | Obrace of int
+  | Oparenthesis of int
+  | Osbracket of int
+  | Percent of int
+  | Plus of int
+  | Question of int
+  | Quote of int
+  | Return of int
+  | Semicolon of int
+  | Slash of int
+  | Space of int
+  | Star of int
+  | Tab of int
+  | Underscore of int
+  | Word of string
+
 let lex_from_string s =
   let result = ref [] in
   let i = ref 0 in
@@ -40,13 +77,13 @@ let lex_from_string s =
     let rec loop () =
       begin
         if !i = l then
-          `Word (String.sub s start (!i-start))
+          Word (String.sub s start (!i-start))
         else
           match s.[!i] with
             | ' ' | '\t' | '\n' | '\r' | '#' | '*' | '-' | '+' | '`' | '\'' | '"' 
             | '\\' | '_' | '[' | ']' | '{' | '}' | '(' | ')' | ':' | ';' | '>' 
             | '<' | '@' | '&' | '|' | '^' | '.' | '/' | '$' | '%' | '!' | '?' -> 
-                `Word (String.sub s start (!i-start))
+                Word (String.sub s start (!i-start))
             | c -> incr i; loop()
       end      
     in
@@ -65,7 +102,7 @@ let lex_from_string s =
         | ' ' | '\t' | '\n' | '\r' | '#' | '*' | '-' | '+' | '`' | '\'' | '"' 
         | '\\' | '_' | '[' | ']' | '{' | '}' | '(' | ')' | ':' | ';' | '>' 
         | '<' | '@' | '&' | '|' | '^' | '.' | '/' | '$' | '%' | '!' | '?' ->
-            `Number(String.sub s start (!i-start))
+            Number(String.sub s start (!i-start))
         | _ ->
             i := start;
             word()
@@ -77,39 +114,39 @@ let lex_from_string s =
     while !i < l do
       () ++
         match s.[!i] with 
-          | ' '  as c  -> incr i; `Space (rcount c)
-          | '\t' as c  -> incr i; `Tab (rcount c)
-          | '\n' as c  -> incr i; `Newline (rcount c)
-          | '\r' as c  -> incr i; `Return (rcount c)
-          | '#'  as c  -> incr i; `Hash (rcount c)
-          | '*'  as c  -> incr i; `Star (rcount c)
-          | '-'  as c  -> incr i; `Minus (rcount c)
-          | '+'  as c  -> incr i; `Plus (rcount c)
-          | '`'  as c  -> incr i; `Backquote (rcount c)
-          | '\'' as c  -> incr i; `Quote (rcount c)
-          | '"'  as c  -> incr i; `Doublequote (rcount c)
-          | '\\' as c  -> incr i; `Backslash (rcount c)
-          | '_'  as c  -> incr i; `Underscore (rcount c)
-          | '['  as c  -> incr i; `Osbracket (rcount c)
-          | ']'  as c  -> incr i; `Csbracket (rcount c)
-          | '{'  as c  -> incr i; `Obrace (rcount c)
-          | '}'  as c  -> incr i; `Cbrace (rcount c)
-          | '('  as c  -> incr i; `Oparenthesis (rcount c)
-          | ')'  as c  -> incr i; `Cparenthesis (rcount c)
-          | ':'  as c  -> incr i; `Colon (rcount c)
-          | ';'  as c  -> incr i; `Semicolon (rcount c)
-          | '>'  as c  -> incr i; `Greaterthan (rcount c)
-          | '<'  as c  -> incr i; `Lessthan (rcount c)
-          | '@'  as c  -> incr i; `At (rcount c)
-          | '&'  as c  -> incr i; `Ampersand (rcount c)
-          | '|'  as c  -> incr i; `Bar (rcount c)
-          | '^'  as c  -> incr i; `Caret (rcount c)
-          | '.'  as c  -> incr i; `Dot (rcount c)
-          | '/'  as c  -> incr i; `Slash (rcount c)
-          | '$'  as c  -> incr i; `Dollar (rcount c)
-          | '%'  as c  -> incr i; `Percent (rcount c)
-          | '!'  as c  -> incr i; `Exclamation (rcount c)
-          | '?'  as c  -> incr i; `Question (rcount c)
+          | ' '  as c  -> incr i; Space (rcount c)
+          | '\t' as c  -> incr i; Tab (rcount c)
+          | '\n' as c  -> incr i; Newline (rcount c)
+          | '\r' as c  -> incr i; Return (rcount c)
+          | '#'  as c  -> incr i; Hash (rcount c)
+          | '*'  as c  -> incr i; Star (rcount c)
+          | '-'  as c  -> incr i; Minus (rcount c)
+          | '+'  as c  -> incr i; Plus (rcount c)
+          | '`'  as c  -> incr i; Backquote (rcount c)
+          | '\'' as c  -> incr i; Quote (rcount c)
+          | '"'  as c  -> incr i; Doublequote (rcount c)
+          | '\\' as c  -> incr i; Backslash (rcount c)
+          | '_'  as c  -> incr i; Underscore (rcount c)
+          | '['  as c  -> incr i; Osbracket (rcount c)
+          | ']'  as c  -> incr i; Csbracket (rcount c)
+          | '{'  as c  -> incr i; Obrace (rcount c)
+          | '}'  as c  -> incr i; Cbrace (rcount c)
+          | '('  as c  -> incr i; Oparenthesis (rcount c)
+          | ')'  as c  -> incr i; Cparenthesis (rcount c)
+          | ':'  as c  -> incr i; Colon (rcount c)
+          | ';'  as c  -> incr i; Semicolon (rcount c)
+          | '>'  as c  -> incr i; Greaterthan (rcount c)
+          | '<'  as c  -> incr i; Lessthan (rcount c)
+          | '@'  as c  -> incr i; At (rcount c)
+          | '&'  as c  -> incr i; Ampersand (rcount c)
+          | '|'  as c  -> incr i; Bar (rcount c)
+          | '^'  as c  -> incr i; Caret (rcount c)
+          | '.'  as c  -> incr i; Dot (rcount c)
+          | '/'  as c  -> incr i; Slash (rcount c)
+          | '$'  as c  -> incr i; Dollar (rcount c)
+          | '%'  as c  -> incr i; Percent (rcount c)
+          | '!'  as c  -> incr i; Exclamation (rcount c)
+          | '?'  as c  -> incr i; Question (rcount c)
           | '0' .. '9' -> maybe_number()
           | c -> word()
     done;
